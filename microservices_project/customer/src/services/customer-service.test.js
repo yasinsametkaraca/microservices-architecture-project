194a42642887dd0ae7a996131494e68767388556
Yasin Samet Karaca <ysk@outlook.com.tr>
2024-09-20 13:27:22 +0300

    refactor: update test case

--- microservices_project/customer/src/services/customer-service.test.js
+++ microservices_project/customer/src/services/customer-service.test.js
@@ -1,11 +1,10 @@
-
 describe("CustomerService", () => { // CustomerService is class name. Which service it is
 
     describe("SignIn", () => { // SignIn is function name. Which function we are testing
 
         test("validate user inputs", () => {}); // Which Scenario we are testing
-
         test("Validate response", async () => {}); // Which Scenario we are testing
+
     });
 
 });
